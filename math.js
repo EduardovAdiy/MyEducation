d369d4e2e1a597a6cbc6500c9eb83359c8c18ff5
Адий Эдуардов <oshakbaev_adi@mail.ru>
2025-09-01 10:45:53 +0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+const e = 2.718;
+
+const square = (x) => {
+  return x * x;
+};
+
+const surfaceArea = (r) => {
+  return 4 * pi * square(r);
+};
+
+export { e, square, surfaceArea};
+export default pi;
