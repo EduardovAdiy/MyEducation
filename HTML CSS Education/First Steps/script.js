b34ed1a938fc66e879a9aa782a8107d28a25363e
Адий Эдуардов <oshakbaev_adi@mail.ru>
2025-09-18 16:24:45 +0700

    Create script.js

---
+++ HTML CSS Education/First Steps/script.js
@@ -0,0 +1,26 @@
+ 'use strict';
+
+ function promtMessage() {
+            const mess =  prompt('Сколько тебе лет?');
+            if(/^[0-9]+$/.test(mess) === true){
+                const conf = confirm(`Тебе точно ${mess} лет?`);
+                if(conf === true){
+                    alert('Возраст подтвержден!');
+                } else alert('Возраст НЕ подтвержден!');
+            } else alert('Я так не играю!');
+        };
+
+function showMessage() {
+            alert('Привет тебе, дружище!');
+        };
+
+function getUserName() {
+    let userName = prompt ('Введите ваше имя: ');
+    if(userName === '' || userName === null){
+        userName = 'неизвестный';
+    }; 
+    alert(`Добрый день, ${userName}!`); 
+    document.getElementById('userName').innerHTML = `Привет, ${userName}!`;
+};
+
+window.onload = getUserName();
