937d875943b15fd178d469cb8ed4297b59f91e5e
Адий Эдуардов <oshakbaev_adi@mail.ru>
2025-09-22 14:56:32 +0700

    Update script.js

--- HTML CSS Education/First Steps/script.js
+++ HTML CSS Education/First Steps/script.js
@@ -17,15 +17,6 @@ function showMessage() {
             alert('Привет тебе, дружище!');
         };
 
-function getUserName() {
-    let userName = prompt ('Введите ваше имя: ');
-    if(userName === '' || userName === null){
-        userName = 'неизвестный';
-    }; 
-    alert(`Добрый день, ${userName}!`); 
-    document.getElementById('userName').innerHTML = `Привет, ${userName}!`;
-};
-
 function goAway() {
     const dates = document.getElementById('imgId').innerHTML;
     if(dates === 'Хомяк уходит'){
@@ -39,4 +30,62 @@ function goAway() {
     }
 };
 
-window.onload = getUserName();
+function getUserName() {
+    let userName = prompt ('Введите ваше имя: ');
+    if(userName === '' || userName === null){
+        userName = 'неизвестный';
+    }; 
+    alert(`Добрый день, ${userName}!`); 
+    document.getElementById('userName').innerHTML = `Привет, ${userName}!`;
+};
+
+function changeRadioValue1 () {
+     document.getElementById('rd1').chacked = false;
+
+};
+
+function changeRadioValue2 () {
+    document.getElementById('rd2').checked = false;
+};
+
+function printTextValueOnChange() {
+    let textValue = document.getElementById("text").value;
+
+    alert (`Вы ввели: ${textValue}`);
+};
+function printCheckBoxValue() {
+    let checkBoxValue = document.getElementById('check').value;
+    let checkConfirm = confirm(`Вы действительно выбрали значение чек-бокса: ${checkBoxValue}`);
+
+    if (checkConfirm === false) {
+        document.getElementById('check').checked = false;
+    };
+};
+
+function printSelectorValue() {
+    let selector = document.getElementById("selector").value;
+
+    alert(`Вы выбрали: Вариант ${selector}`);
+
+    let inTagElementDates = `Вы указали: <br>
+Текст: ${document.getElementById ('text')} <br>
+Пол: ${getUserSex (document.getElementById('rd1').value, document.getElementById('rd2').value)}
+Чек-бокс: ${document.getElementById('check').value}
+Вариант из списка: Вариант ${document.getElementById("selector").value}`
+
+    document.getElementById('submitDates').innerHTML = inTagElementDates;
+};
+
+function submitDates () {
+    alert('Данные успешно отправлены!\n' + printSelectorValue());
+};
+
+function getUserSex (x, y) {
+    if (x === true) {
+        return 'мужской';
+    } else if (y === true) {
+        return 'женский';
+    } else {
+        return 'не указан';
+    }
+};
